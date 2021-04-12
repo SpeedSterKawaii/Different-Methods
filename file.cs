c6494e41f9581b40ac4beecd8f3d2516135b105d
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-04-12 10:22:20 -0400

    Create file.cs

---
+++ file.cs
@@ -0,0 +1,23 @@
+public void Execute(string method, string script)
+{
+    if (method == "Wrapper") // They are choosing wrapper
+    {
+        //  Execute via "Wrapper Pipe"
+    }
+    else if (method == "Bytecode")
+    {
+        //  Execute via "Bytecode Pipe"
+    }
+    else if (string.IsNullOrEmpty(method))
+    {
+        // This is a empty string.
+        MessageBox.Show("String throwed NullOrEmpty Exception.","Method");
+    }
+    else
+    {
+        /*
+            Not a method and cannot continue.
+        */
+        MessageBox.Show("This is not a method.","Method");
+    }
+}
